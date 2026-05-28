a6c4627a4336f6cf584cd72a5f011142a32b7442
KattKriger <31779575+KattKriger@users.noreply.github.com>
2026-05-28 19:43:15 +0000

    django setup and reports model

---
+++ reports/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from django.db import models
+
+class Report(models.Model):
+    year = models.IntegerField()
+    month = models.CharField(max_length=20)
+
+    internal_castration = models.IntegerField(null=True, blank=True)
+    external_castration = models.IntegerField(null=True, blank=True)
+
+    internal_attendance = models.IntegerField(null=True, blank=True)
+    external_attendance = models.IntegerField(null=True, blank=True)
+    
+    deaths = models.IntegerField(null=True, blank=True)
+    euthanasia = models.IntegerField(null=True, blank=True)
+
+    samuvet_requests = models.IntegerField(null=True, blank=True)
+
+    animal_donations = models.IntegerField(null=True, blank=True)
+
+    releases = models.IntegerField(null=True, blank=True)
+
+    rescue_animals = models.IntegerField(null=True, blank=True)
+
+    sheltered_animals = models.IntegerField(null=True, blank=True)
+
+    def _str_(self):
+        return f"(self.year) - (self.month)"
---
+++ reports/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Report
+
+def report_list(request):
+    reports = Report.objects.all()
+
+    return render(request, 'reports/report_list.html', {'reports': reports
+    })
