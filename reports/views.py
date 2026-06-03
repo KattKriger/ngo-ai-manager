94836a0fd696ca7d6c3b2e73ab9809f3a3de9ce9
KattKriger <31779575+KattKriger@users.noreply.github.com>
2026-06-03 18:44:08 +0000

    Added dashboard KPIs

--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,7 @@
 from .models import Report
 from django.shortcuts import render, redirect
 from .forms import ReportForm
+from django.db.models import Sum
 
 
 def report_list(request):
@@ -8,17 +9,34 @@ def report_list(request):
     selected_year = request.GET.get('year')
 
     reports = Report.objects.all()
+    total_reports = reports.count()
+    total_internal_attendance = reports.aggregate(
+        Sum('internal_attendance')
+        )['internal_attendance__sum']
+    total_external_attendance = reports.aggregate(
+        Sum('external_attendance')
+        )['external_attendance__sum']
+    total_deaths = reports.aggregate(
+        Sum('deaths')
+        )['deaths__sum']
 
     if selected_year:
             reports = reports.filter(year=selected_year)
         
-    years = Report.objects.values_list('year', flat=True).distinct().order_by('year')
+    years = Report.objects.values_list(
+        'year',
+        flat=True
+    ).distinct().order_by('year')
 
     return render(request, 'reports/report_list.html', {
         'reports': reports,
         'years': years,
-        'selected_year': selected_year
-        })
+        'selected_year': selected_year,
+        'total_reports': total_reports,
+        'total_internal_attendance': total_internal_attendance,
+        'total_external_attendance': total_external_attendance,
+        'total_deaths': total_deaths,
+    })
 
 def create_report(request):
     if request.method == 'POST':
